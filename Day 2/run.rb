7d6ee9088ccd295f4e07bba975dfc6500bccdbeb
Patrycja Król <pkrol@northpass.com>
2021-12-03 13:43:20 +0100

    Add part 2 of Day 2

--- Day 2/run.rb
+++ Day 2/run.rb
@@ -23,6 +23,24 @@ class DayTwo
     puts calculate_product_of_position(position_x, position_y)
   end
 
+  def calculate_advanced_position
+    position_x = 0
+    position_y = 0
+    aim = 0
+
+    data.each do |move|
+      direction = move[0].split(' ')[0]
+      value = move[0].split(' ')[1].to_i
+
+      value = 0 - value if direction == 'up'
+      aim += value if direction == 'up' or direction == 'down'
+
+      position_x += value if direction == 'forward'
+      position_y += value * aim if direction == 'forward'
+    end
+    puts calculate_product_of_position(position_x, position_y)
+  end
+
   private
 
   attr_reader :filename, :data
@@ -43,3 +61,4 @@ class DayTwo
 end
 
 DayTwo.new('data.csv').calculate_position
+DayTwo.new('data.csv').calculate_advanced_position
