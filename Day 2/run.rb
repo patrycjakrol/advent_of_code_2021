ba22ca15533b1c9c9143db3094a7827b124aff22
Patrycja Król <pkrol@northpass.com>
2021-12-03 13:11:42 +0100

    Add part 1 of Day 2

---
+++ Day 2/run.rb
@@ -0,0 +1,45 @@
+require 'csv'
+require 'pry'
+
+class DayTwo
+  def initialize(filename)
+    @filename = filename
+    post_initialize
+  end
+
+  def calculate_position
+    position_x = 0
+    position_y = 0
+
+    data.each do |move|
+      direction = move[0].split(' ')[0]
+      value = move[0].split(' ')[1].to_i
+
+      value = 0 - value if direction == 'up'
+
+      position_x += value if direction == 'forward'
+      position_y += value if direction == 'up' or direction == 'down'
+    end
+    puts calculate_product_of_position(position_x, position_y)
+  end
+
+  private
+
+  attr_reader :filename, :data
+
+  def post_initialize
+    unless File.exist?(filename)
+      puts "Given file doesn't exist."
+      exit
+    end
+
+    content = File.read(filename)
+    @data = CSV.parse(content)
+  end
+
+  def calculate_product_of_position(position_x, position_y)
+    position_x * position_y
+  end
+end
+
+DayTwo.new('data.csv').calculate_position
